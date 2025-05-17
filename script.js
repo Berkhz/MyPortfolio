eda37074281bf06a13eb94888af9b025a778fff9
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-05-17 16:58:19 -0300

    up

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleDarkMode() {
+  document.body.classList.toggle('dark');
+}
+
+function trocarIdioma() {
+  window.location.href = 'en.html';
+}
+
+function switchLanguage() {
+  window.location.href = 'index.html';
+}
